cc068038d8e5e2d61180be185e02b25c8006c57e
Henrik Stiernstedt <henrik.stiernstedt@multisoft.se>
2022-02-07 23:11:30 +0100

    Cleanup and auto-render

--- autodocGraph.js
+++ autodocGraph.js
@@ -2,16 +2,17 @@
 var vm = new Vue({
   el: '#app',
   data: {
-		
-		
 		prock: { 
 			"Info": {"object_id":576109193,"Name":"Appointment.Appointment_API_InsertUpdate_FieldByField","Length":10549,"SchemaName":"Appointment","ObjectName":"Appointment_API_InsertUpdate_FieldByField"}, "MetaDataReference": [{}], 
 			"Calls": [{"object_id":1147919211,"Name":"Appointment.AppointmentStatus_API_GetIdForCancelled","SchemaName":"Appointment","ObjectName":"AppointmentStatus_API_GetIdForCancelled"},{"object_id":1184111359,"Name":"Appointment.Appointment_InsertUpdate_Internal","SchemaName":"Appointment","ObjectName":"Appointment_InsertUpdate_Internal"},{"object_id":1380968046,"Name":"Room.Room_API_All","SchemaName":"Room","ObjectName":"Room_API_All"},{"object_id":1985494202,"Name":"Util.User_GetIdForScheduledJob","SchemaName":"Util","ObjectName":"User_GetIdForScheduledJob"}],
 			"ReferencedBy":[{"object_id":121155577,"Name":"AppointmentIntegration.CreateAppointment","SchemaName":"AppointmentIntegration","ObjectName":"CreateAppointment"},{"object_id":185155805,"Name":"AppointmentIntegration.UpdateAppointment","SchemaName":"AppointmentIntegration","ObjectName":"UpdateAppointment"},{"object_id":544109079,"Name":"Appointment.Appointment_API_ConnectToAppointmentGroup_Internal","SchemaName":"Appointment","ObjectName":"Appointment_API_ConnectToAppointmentGroup_Internal"},{"object_id":858642302,"Name":"Appointment.Web_Appointment_NumberOfParticipants_Update","SchemaName":"Appointment","ObjectName":"Web_Appointment_NumberOfParticipants_Update"},{"object_id":880110276,"Name":"Appointment.Appointment_AppointmentStatus_InsertUpdate","SchemaName":"Appointment","ObjectName":"Appointment_AppointmentStatus_InsertUpdate"},{"object_id":944110504,"Name":"Appointment.Appointment_Cancel","SchemaName":"Appointment","ObjectName":"Appointment_Cancel"},{"object_id":956634551,"Name":"Appointment.Web_Appointment_CustomerNumber_Update","SchemaName":"Appointment","ObjectName":"Web_Appointment_CustomerNumber_Update"},{"object_id":1004634722,"Name":"Appointment.Web_Appointment_Insert","SchemaName":"Appointment","ObjectName":"Web_Appointment_Insert"},{"object_id":1617597001,"Name":"Appointment.AppointmentSeries_Appointment_EditableGrid","SchemaName":"Appointment","ObjectName":"AppointmentSeries_Appointment_EditableGrid"},{"object_id":1776113468,"Name":"Appointment.AppointmentSeries_Appointment_InsertUpdate_Internal_AddOn","SchemaName":"Appointment","ObjectName":"AppointmentSeries_Appointment_InsertUpdate_Internal_AddOn"},{"object_id":1849877757,"Name":"ExchangeSynchronisation.AppointmentImport","SchemaName":"ExchangeSynchronisation","ObjectName":"AppointmentImport"}]},
-		
- 
+
 		status: {
-				"CurrentItem" : "Appointment.Appointment_API_InsertUpdate_FieldByField",
+				"CurrentItem" : 
+					{
+					"schema": "Appointment",
+					"name": "Appointment_API_InsertUpdate_FieldByField"
+					},
 				"Database":	"Stortinget"
 		}
 	},
@@ -39,22 +40,34 @@ var vm = new Vue({
 		},
 		
 		GetProc: function(schema, name) {
-		var newProck = null;
-		status = $.ajax({
-			dataType: "json",
-			url: 'http://pegasus/pegasus_demo/Multirest/AutoDoc/Stortinget_Bokningssystem/'+schema+'/'+name,
-			data: null,
-			success: function(prockInfo) {
-				vm.prock = (prockInfo);
-				vm.status.CurrentItem = schema+'/'+name;
-				console.log(prockInfo);
-			},
-			error: function(prockInfo) {
-				console.error("Failed to load prockInfo");
-			}
-    });
-   }
-  }  
+			console.log("Getting " + schema + "." + name)
+			var newProck = null;
+			status = $.ajax({
+				dataType: "json",
+				url: 'http://pegasus/pegasus_demo/Multirest/AutoDoc/Stortinget_Bokningssystem/'+schema+'/'+name,
+				data: null,
+				success: function(prockInfo) {
+					vm.prock = (prockInfo);
+					vm.status.CurrentItem.schema = schema;
+					vm.status.CurrentItem.name = name;
+					console.log(prockInfo);
+					setTimeout(vm.RenderGraph, 100); // TODO: Find a better way to "wait" for vue to render the mermaid input data
+				},
+				error: function(prockInfo) {
+					console.error("Failed to load prockInfo");
+				}
+			});
+   		},
+		RenderGraph: function() {
+			var input = document.getElementById("input");
+			input.value = $('#rawValue').text();
+			
+			var output = document.getElementById("output");
+			mermaid.render('theGraph', input.value, function(svgCode) {
+				output.innerHTML = svgCode;
+			});
+		}
+  	}  
 });
 
 $(function(){
