5d40ce7966f5cba9a5ee6d1402670809f69ce89a
Henrik Stiernstedt <henrik.stiernstedt@multisoft.se>
2022-03-15 23:29:38 +0100

    Display of code definition.

--- autodocGraph.js
+++ autodocGraph.js
@@ -3,7 +3,8 @@ var vm = new Vue({
   el: '#app',
   data: {
 		prock: { 
-			"Info": {"object_id":576109193,"Name":"Appointment.Appointment_API_InsertUpdate_FieldByField","Length":10549,"SchemaName":"Appointment","ObjectName":"Appointment_API_InsertUpdate_FieldByField"}, "MetaDataReference": [{}], 
+			"Info": {"object_id":576109193,"Name":"Appointment.Appointment_API_InsertUpdate_FieldByField","Length":10549,"SchemaName":"Appointment","ObjectName":"Appointment_API_InsertUpdate_FieldByField", "definition": ""}, 
+			"MetaDataReference": [{}], 
 			"Calls": [{"object_id":1147919211,"Name":"Appointment.AppointmentStatus_API_GetIdForCancelled","SchemaName":"Appointment","ObjectName":"AppointmentStatus_API_GetIdForCancelled"},{"object_id":1184111359,"Name":"Appointment.Appointment_InsertUpdate_Internal","SchemaName":"Appointment","ObjectName":"Appointment_InsertUpdate_Internal"},{"object_id":1380968046,"Name":"Room.Room_API_All","SchemaName":"Room","ObjectName":"Room_API_All"},{"object_id":1985494202,"Name":"Util.User_GetIdForScheduledJob","SchemaName":"Util","ObjectName":"User_GetIdForScheduledJob"}],
 			"ReferencedBy":[{"object_id":121155577,"Name":"AppointmentIntegration.CreateAppointment","SchemaName":"AppointmentIntegration","ObjectName":"CreateAppointment"},{"object_id":185155805,"Name":"AppointmentIntegration.UpdateAppointment","SchemaName":"AppointmentIntegration","ObjectName":"UpdateAppointment"},{"object_id":544109079,"Name":"Appointment.Appointment_API_ConnectToAppointmentGroup_Internal","SchemaName":"Appointment","ObjectName":"Appointment_API_ConnectToAppointmentGroup_Internal"},{"object_id":858642302,"Name":"Appointment.Web_Appointment_NumberOfParticipants_Update","SchemaName":"Appointment","ObjectName":"Web_Appointment_NumberOfParticipants_Update"},{"object_id":880110276,"Name":"Appointment.Appointment_AppointmentStatus_InsertUpdate","SchemaName":"Appointment","ObjectName":"Appointment_AppointmentStatus_InsertUpdate"},{"object_id":944110504,"Name":"Appointment.Appointment_Cancel","SchemaName":"Appointment","ObjectName":"Appointment_Cancel"},{"object_id":956634551,"Name":"Appointment.Web_Appointment_CustomerNumber_Update","SchemaName":"Appointment","ObjectName":"Web_Appointment_CustomerNumber_Update"},{"object_id":1004634722,"Name":"Appointment.Web_Appointment_Insert","SchemaName":"Appointment","ObjectName":"Web_Appointment_Insert"},{"object_id":1617597001,"Name":"Appointment.AppointmentSeries_Appointment_EditableGrid","SchemaName":"Appointment","ObjectName":"AppointmentSeries_Appointment_EditableGrid"},{"object_id":1776113468,"Name":"Appointment.AppointmentSeries_Appointment_InsertUpdate_Internal_AddOn","SchemaName":"Appointment","ObjectName":"AppointmentSeries_Appointment_InsertUpdate_Internal_AddOn"},{"object_id":1849877757,"Name":"ExchangeSynchronisation.AppointmentImport","SchemaName":"ExchangeSynchronisation","ObjectName":"AppointmentImport"}]},
 
@@ -81,7 +82,13 @@ var vm = new Vue({
 				.replaceAll("'", '&#039;')
 				.replaceAll('(', '').replaceAll(')', '')
 				.replaceAll('[', '').replaceAll(']', '');
+		},
+		FormatSqlCode : function(definition) {
+			return definition
+				.replaceAll("\r\n", "<br />")
+				.replaceAll("\t", "    ");
 		}
+
   	}  
 });
 
---
+++ readme.md
@@ -0,0 +1,18 @@
+## Sodftadmin code browser - SCB
+This is only half of the project!
+To run this site, you need a web service that returns the data in exactly the right way.
+
+## Feature list
+* Display all code objects in a data base, with its relation to all abjects calling this object and all beeing called from it. MenuItems and other Softadmin usages are also shown.
+* Possibility to use all databases on the server based on query parameters.
+
+
+## Feature wish list
+* Add code highlighning
+* Add version history browsing (Internal Softadmin version history)
+* List/search code objects in database
+* List/search databases on server
+* Precalculation and display of all objects beeing called from the active code object, so that we can see the "weight" of all calls. For example, the total number of rows hidden behind a stored procedure.
+* Link to open an object in SSMS.
+* Stand alone version build into or easily installed into Softadmin
+* All security vulnerabilities fixed.
